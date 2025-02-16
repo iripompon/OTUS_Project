ab0f463702d8302bd61e5c97e3b606fc0dea57b5
iripompon <iripompon@yandex.ru>
2025-02-16 17:36:13 +0300

    1

--- internal/model/model_87_88.go
+++ internal/model/model_87_88.go
@@ -1,6 +1,7 @@
 package model
 
 //message 87 object InsuredPersonResponse
+//message 88 object InsuredPersonMismatch
 
 type Protocol struct {
 	item []string
@@ -10,5 +11,10 @@ type InsuredPersonResponse struct {
 	ResponseOn string
 	Snils      string
 	Status     string
+	Protocol
+}
+
+type InsuredPersonMismatch struct {
+	Snils string
 	Protocol
 }
